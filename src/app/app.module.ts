dcc9ce6feba0a1c7aca018346d1f03c82b1fc867
Dhyainnovations <you@example.com>
2022-03-16 14:17:43 +0530

    First Push

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },GooglePlus],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
