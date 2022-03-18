ca2ea29688f30c34326f9019f41b775197b158d4
Dhyainnovations <you@example.com>
2022-03-18 11:11:56 +0530

    Notif Push 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { LocalNotifications, LocalNotificationSchema } from '@capacitor/local-notifications';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  notificationsDelayInSeconds: string = "2";
+  constructor() { }
 
-  constructor( ) { }
 
+  ScheduleNotification() {
+    var options: LocalNotificationSchema = {
+      id: 123,
+      title: "title",
+      body: "Body",
+      summaryText: "summaryText",
+      largeBody: "largeBody",
+      schedule: { at: new Date(new Date().getTime() + parseInt(this.notificationsDelayInSeconds) * 1000) },
+      extra: "extra"
+    }
+    LocalNotifications.schedule({ notifications: [options] }).then(() => {
+      alert("Notification");
+    })
+  }
 }
